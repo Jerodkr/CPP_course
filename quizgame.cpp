3722707a8a6f849c21da1fa5019daa659f73b59b
Stefan de Kraker <stefandekraker1999@gmail.com>
2022-09-29 20:31:14 +0200

    Reformatted code

--- quizgame.cpp
+++ quizgame.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(){
+int main() {
 
     std::string vragen[] = {"1. Wat is het liefste dier?: ",
                             "2. Wat voor dier is de Cock?: ",
@@ -16,40 +16,31 @@ int main(){
 
     char antwoorden[] = {'C', 'D', 'C', 'B', 'A'};
 
-    int size = sizeof(vragen)/sizeof(vragen[0]);
+    int size = sizeof(vragen) / sizeof(vragen[0]);
     int gok;
-    int score;
+    int score = 0;
 
-    for(int i = 0; i < size; i++){
-            std::cout << "**************************************" << '\n';
-            std::cout << vragen[i] << "\n";
-            std::cout << "**************************************" << '\n';
+    for (int i = 0; i < size; i++) {
+        std::cout << "**************************************" << '\n';
+        std::cout << vragen[i] << "\n";
+        std::cout << "**************************************" << '\n';
 
-        for(int j = 0; j < sizeof(keuzes[i])/sizeof(keuzes[i][0]); j++){
-             std::cout << keuzes[i][j] << "\n";
-                }
+        for (int j = 0; j < sizeof(keuzes[i]) / sizeof(keuzes[i][0]); j++) {
+            std::cout << keuzes[i][j] << "\n";
+        }
 
         gok = std::cin.get();               // get one character
         std::cin.ignore(1, '\n');  // ignore until enter
-        std::cout << gok << '\n';
         gok = toupper(gok);
 
-
-        if(gok == antwoorden[i]){
-            std::cout << "CORRECT!";
+        if (gok == antwoorden[i]) {
+            std::cout << "CORRECT!" << '\n';
             score++;
-        }
-        else{
-
-            std::cout << "False!\n";
+        } else {
+            std::cout << "False!" << '\n';
             std::cout << "Correct answer: " << antwoorden[i] << '\n';
         }
-    
-        
     }
-    
-
- return 0;
-
-
+    // TODO: maybe print the amount of correct answers?!
+    return 0;
 }
